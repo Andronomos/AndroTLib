a2bc867e5d285907572ad789d25492b66041309e
Andronomos <andronomos@proton.me>
2024-10-27 19:19:17 -0700

    Added utility class for sending messages in chat

---
+++ Utils/BroadcastUtils.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.Chat;
+using Terraria.Localization;
+
+namespace AndroTLib.Utils;
+
+public static class BroadcastUtils
+{
+    public static void BroadcastError(string message)
+    {
+        BroadcastChatMessage(message, Color.Red);
+    }
+
+    public static void BroadcastInfo(string message)
+    {
+        BroadcastChatMessage(message, Color.Yellow);
+    }
+
+    public static void BroadcastWarning(string message)
+    {
+        BroadcastChatMessage(message, Color.Yellow);
+    }
+
+    private static void BroadcastChatMessage(string message, Color color)
+    {
+        ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(message), color);
+    }
+}
