c81add2c2911671748f292ea911353a4250c14e4
Andronomos <andronomos@proton.me>
2024-11-03 01:48:24 -0800

    Cleaned up ocean and underworld logic

--- Utils/PlayerUtils.cs
+++ Utils/PlayerUtils.cs
@@ -30,7 +30,7 @@ public static class PlayerUtils
                 HandleMeteoriteTeleport(player);
                 break;
             case PointOfInterest.Ocean:
-                HandleOceanTeleport(player);
+                player.MagicConch();
                 break;
             case PointOfInterest.Spawn:
                 HandleSpawnTeleport(player);
@@ -39,7 +39,7 @@ public static class PlayerUtils
                 HandleTempleTeleport(player);
                 break;
             case PointOfInterest.Underworld:
-                HandleUnderworldTeleport(player);
+                player.DemonConch();
                 break;
         }
     }
@@ -71,11 +71,6 @@ public static class PlayerUtils
 
     }
 
-    private static void HandleOceanTeleport(Player player)
-    {
-        player.MagicConch();
-    }
-
     internal static void HandleSpawnTeleport(Player player)
     {
         Teleport(player, new(Main.spawnTileX, Main.spawnTileY));
@@ -84,11 +79,6 @@ public static class PlayerUtils
     internal static void HandleTempleTeleport(Player player)
     {
 
-    }
-
-    internal static void HandleUnderworldTeleport(Player player)
-    {
-        player.DemonConch();
     }
 
     internal static void Teleport(Player player, Vector2 destination)
